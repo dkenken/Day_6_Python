ef6f8e8667cb152698824da024482720a53d6277
dkenken <dkenken72@gmail.com>
2023-02-26 22:25:58 -0800

    Day_6_Python

---
+++ day_6.py
@@ -0,0 +1,33 @@
+# Exercises Day #6
+
+#AGENDA: #Code Blocks #Functions #While Loops
+
+# NOTE: I will be using "Reeborg World" to solve most of the exercises.
+# This should be a small file for Day #6. 
+
+# Functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
+
+# Indentation (Identation is very important!)
+
+# Example
+sky = ''
+def my_indentation():
+    if sky == "clear":
+        print("Blue")
+    elif sky == "cloudy":
+        print("Grey")
+    print("Hello")
+print("World")
+
+# While Loop
+
+while something_is_true:
+    # Do something repeatedly
+
